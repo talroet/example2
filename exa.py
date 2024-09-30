6bb20e058e839499698809a0b07b4f745c27e026
talroet <talroet8@gmail.com>
2024-09-30 11:53:37 +0300

    add exa.py

---
+++ exa.py
@@ -0,0 +1,13 @@
+#1
+print(" Hello Enter your  name: ")
+
+#2
+print(" Hello Enter your adress: ")
+
+
+
+"""
+name = "Name" 
+print(" Hello " , name ," How are you: " )
+"""
+print('My name is Tal and I am from Shaarei Tikva')
